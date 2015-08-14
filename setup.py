82355f7a16a733b025d1f09c62dbd081d5506c92
lexual <lex@jbadigital.com>
2015-08-14 05:46:43 +0000

    update setup.py

--- setup.py
+++ setup.py
@@ -1,1 +1,23 @@
-# TODO: write me
+# -*- coding: utf-8 -*-
+
+from setuptools import setup
+# from setuptools import find_packages
+
+
+with open('README.md') as f:
+    readme = f.read()
+
+with open('LICENSE') as f:
+    license = f.read()
+
+setup(
+    name='rocket-fuel-sdk-rest',
+    version='0.0.1',
+    description='ExactTarget REST API Wrapper',
+    long_description=readme,
+    author='JBA',
+    author_email='lex@jbadigital.com.com',
+    url='https://github.com/jbadigital/rocket-fuel-sdk-rest',
+    license=license,
+    packages=['rocket_fuel_sdk_rest']
+)
